cb2039cca5abe39fc128e8fdc291e9c296ce5bb8
Emmyadams007 <87890378+Emmyadams007@users.noreply.github.com>
2021-09-12 20:00:24 -0400

    Add files via upload

---
+++ public/Members.js
@@ -0,0 +1,23 @@
+const members = [
+  {
+    id: 1,
+    name: 'Emmy Adams',
+    email: 'emmy@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Bob Wild',
+    email: 'wild@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Adams Adams',
+    email: 'adams@gmail.com',
+    status: 'active'
+  },
+
+];
+
+module.exports = members;
